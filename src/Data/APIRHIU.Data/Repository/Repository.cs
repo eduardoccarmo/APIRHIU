d127ab06eafc7718e4a8ed4130cd31759673801d
eduardoccarmo <eduardo.ccarmo@outlook.com>
2024-04-03 23:25:12 -0300

    Adicionar arquivos de projeto.

---
+++ src/Data/APIRHIU.Data/Repository/Repository.cs
@@ -0,0 +1,58 @@
+﻿using APIRHIU.Data.Context;
+using APIRHIU.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+
+namespace APIRHIU.Core.DomainObjects
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected ApiDbContext Context;
+        protected DbSet<TEntity> DbSet;
+
+        protected Repository(ApiDbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();         
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+           DbSet.Add(entity);
+           await SaveChanges();             
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            DbSet.Remove( new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Context.SaveChangesAsync();
+        }
+
+        public virtual async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();                
+        }
+    }
+}
