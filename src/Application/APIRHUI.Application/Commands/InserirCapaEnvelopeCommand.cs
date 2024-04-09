8273c0804b23dfcf961e664b8a74202b32a9e97b
eduardoccarmo <eduardo.ccarmo@outlook.com>
2024-04-08 22:39:22 -0300

    Criando servico para processamento das rotinas de integração.

---
+++ src/Application/APIRHUI.Application/Commands/InserirCapaEnvelopeCommand.cs
@@ -0,0 +1,20 @@
+﻿using APIRHIU.Core.Message;
+
+namespace APIRHUI.Application.Commands
+{
+    public class InserirCapaEnvelopeCommand : Command
+    {
+        
+        
+        
+        
+        
+        public override bool EhValido()
+        {
+            ValidationResult = new FluentValidation.Results.ValidationResult();
+
+            return ValidationResult.IsValid;
+        }
+    }
+
+}
